cf0e9874b5d9a603be97db830105c3858286ad4e
mkarimov <mkarimov@avtodoria.ru>
2019-04-08 15:38:46 +0300

    0.3.0-RELEASE

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
-(defproject org.clojars.mkarimov/clj-nyam "0.3.0-SNAPSHOT"
+(defproject org.clojars.mkarimov/clj-nyam "0.3.0-RELEASE"
   :description "Simple properties reader"
   :url "https://github.com/MaratKarimov/clj-nyam"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :deploy-repositories {"clojars-https" {:url "https://clojars.org/repo" :sign-releases false}}
   :profiles {:uberjar {:aot :all}})
